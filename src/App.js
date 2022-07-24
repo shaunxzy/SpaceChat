92bb4938d9e013d789a08b48387c9f68bc941737
xloc <xloc.cc@outlook.com>
2022-07-24 16:56:38 -0700

    upload avatar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignupPage from "./auth/SignupPage";
 import LoginPage from "./auth/LoginPage";
 import Verification from "./auth/Verification";
@@ -8,22 +8,24 @@ import UserPage from "./components/UserPage";
 import AuthProvider from "./context/AuthProvider";
 import Visitor from "./components/Visitor";
 import VisitorAuth from "./auth/VisitorAuth";
+import AvatarModal from './components/AvatarModal';
 
 function App() {
-  return (
-      <AuthProvider>
-          <Routes>
-              <Route path={"/"} exact={true} element={<Navigate to={'/home'} />}/>
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/verification" element={<Verification />} />
-              <Route path={"/home/*"} element={<Homepage />} />
-              <Route path={"/user/:id"} element={<UserPage />} />
-              <Route path={"/visitor/:id"} element={<Visitor />}/>
-              <Route path={"/visitor/auth/:id"} element={<VisitorAuth />} />
-          </Routes>
-      </AuthProvider>
-  )
+    return (
+        <AuthProvider>
+            <Routes>
+                <Route path={"/"} exact={true} element={<Navigate to={'/home'} />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/verification" element={<Verification />} />
+                <Route path={"/home/*"} element={<Homepage />} />
+                <Route path={"/user/:id"} element={<UserPage />} />
+                <Route path={"/visitor/:id"} element={<Visitor />} />
+                <Route path={"/visitor/auth/:id"} element={<VisitorAuth />} />
+                <Route path="/test" element={<AvatarModal />} />
+            </Routes>
+        </AuthProvider>
+    );
 }
 
 export default App;
