092f4a730e8486e3b759963da1e44f76e95529de
shaunxzy <zyxuxmu@gmail.com>
2022-08-07 15:05:13 -0700

    set storage

--- src/firebase/customFirebase.js
+++ src/firebase/customFirebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 import {getDatabase} from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
+import { getStorage } from "firebase/storage";
 
 
 const app = initializeApp(config.firebaseConfig)
@@ -12,4 +13,5 @@ export const auth = getAuth(app);
 export const db = getDatabase(app);
 export const analytics = getAnalytics(app);
 export const messaging = getMessaging(app);
+export const storage = getStorage(app);
 export default app;
